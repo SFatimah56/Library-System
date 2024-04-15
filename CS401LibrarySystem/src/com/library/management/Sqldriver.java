0690c66fe33e9ad22288394cd903544564d7de4b
Sharan-Robinson <sharanrobinson2003@gmail.com>
2024-04-15 16:14:48 -0700

    sql update events

--- CS401LibrarySystem/src/com/library/management/Sqldriver.java
+++ CS401LibrarySystem/src/com/library/management/Sqldriver.java
@@ -7,6 +7,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
+import com.library.management.EventsManager.Event;
+
 public class Sqldriver
     {
       public ArrayList<User> getUsers()
@@ -47,6 +49,47 @@ public class Sqldriver
         }
         
         return users;
+      }
+      
+      public ArrayList<Event> getEvents()
+      {
+    	  
+    	  ArrayList<Event> events = new ArrayList<Event>();
+
+        // NOTE: Connection and Statement are AutoClosable.
+        //       Don't forget to close them both in order to avoid leaks.
+        try
+        (
+          // create a database connection
+          Connection connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+          Statement statement = connection.createStatement();
+        )
+        {
+          statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+          ResultSet rs = statement.executeQuery("select id, title, description, eventdate, eventtype from Users");
+          while(rs.next())
+          {
+            // read the result set
+        	Event event = new Event(
+        			rs.getString("title"),
+        			rs.getString("description"),
+        			rs.getDate("eventdate"),
+        			rs.getString("eventtype")
+        	);
+        	
+        	events.add(event);
+
+          }
+        }
+        catch(SQLException e)
+        {
+          // if the error message is "out of memory",
+          // it probably means no database file is found
+          e.printStackTrace(System.err);
+        }
+        
+        return events;
       }
       
       
