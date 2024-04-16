177744a1deaf975307d5b874c5a1609eaa7c0353
Sharan-Robinson <sharanrobinson2003@gmail.com>
2024-04-16 15:17:52 -0700

    operational users.

--- CS401LibrarySystem/src/com/library/management/SocialNetwork.java
+++ CS401LibrarySystem/src/com/library/management/SocialNetwork.java
@@ -10,18 +10,23 @@ import java.sql.Statement;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.sql.Date;
 
 import com.library.management.EventsManager.Event;
+import com.library.management.UserManager.User;
 public class SocialNetwork {
 	EventsManager eventmanager;
+	UserManager usermanager;
 	Scanner scanner;
 	
 	
 	public SocialNetwork(){
 		this.eventmanager = new EventsManager();
+		this.usermanager = new UserManager();
 		getEvents();
+		getUsers();
 	    scanner = new Scanner(System.in);
 
 	}
@@ -73,6 +78,69 @@ public class SocialNetwork {
 	}
 	
 	
+	public void updateUsers() {
+		ArrayList<User> users = usermanager.getAllUsers();
+
+        try
+        (
+          // create a database connection
+          Connection connection = DriverManager.getConnection("jdbc:sqlite:database.db");
+          Statement statement = connection.createStatement();
+        )
+        {
+        	String setUp = "CREATE TABLE IF NOT EXISTS Users (\n"
+        			+ " id integer PRIMARY KEY,\n"
+        			+ " username text NOT NULL,\n"
+        			+ " favoriteBookISBN text,\n"
+        			+ " readingHabits text,\n"
+        			+ " genres text,\n"
+        			+ " followerIDs text,\n"
+        			+ " followingIDs text\n"
+        			+ ");";
+            
+            statement.execute(setUp);
+
+            
+            String sql2 = "INSERT INTO Users(id, username, favoriteBookISBN, readingHabits, genres, followerIDs, followingIDs) VALUES(?, ?, ?, ?, ?, ?, ?) ";
+
+            for(User i : users) {
+                PreparedStatement pstmt = connection.prepareStatement(sql2);
+                pstmt.setInt(1, i.getID());
+                pstmt.setString(2, i.getUsername());
+                pstmt.setString(3, 
+                		String.join(",",i.getFavoriteBookISBNs())
+                		);
+                pstmt.setString(3, 
+                		String.join(",",i.getFavoriteBookISBNs())
+                		);
+                pstmt.setString(4, 
+                		String.join(",",i.getReadingHabits())
+                		);
+                pstmt.setString(5, 
+                		String.join(",",i.getGenres())
+                		);
+                pstmt.setString(6, 
+                		String.join(",",i.getFollowers())
+                		);
+                pstmt.setString(7, 
+                		String.join(",",i.getFollowings())
+                		);
+               
+                pstmt.execute();
+  
+            }
+          
+        }
+        catch(SQLException e)
+        {
+          // if the error message is "out of memory",
+          // it probably means no database file is found
+          e.printStackTrace(System.err);
+        }
+	
+	}
+	
+	
 	public void createEvent()
 	{
 		
@@ -86,6 +154,16 @@ public class SocialNetwork {
         
         eventmanager.createEvent(title, description, date, type);
         updateEvents();
+	}
+	
+	public void createUser()
+	{
+		
+        System.out.print("Enter username: ");
+        String username = scanner.nextLine();
+        
+        usermanager.createUser(username);
+        updateUsers();
 	}
 
     public void getEvents()
@@ -135,6 +213,71 @@ public class SocialNetwork {
       }
       
     }
+    
+    
+    public void getUsers()
+    {
+  	  
+      // NOTE: Connection and Statement are AutoClosable.
+      //       Don't forget to close them both in order to avoid leaks.
+      try
+      (
+        // create a database connection
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:database.db");
+        Statement statement = connection.createStatement();
+      )
+      {
+        statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+        ResultSet resultSet = statement.executeQuery("select * from Users");
+        while(resultSet.next())
+        {
+          // read the result set
+        
+        int id = resultSet.getInt("id");
+        boolean dup = false;
+        
+        for(User i : usermanager.getAllUsers()) {
+        	if(i.getID() == id) {
+        		dup = true;
+        	}
+        }
+        	
+        if(!dup) {
+        usermanager.createUser(
+        		id,
+        		resultSet.getString("username"),
+                new ArrayList<String>(Arrays.asList(resultSet.getString("favoriteBookISBN").split(","))),
+                new ArrayList<String>(Arrays.asList(resultSet.getString("readingHabits").split(","))),
+                new ArrayList<String>(Arrays.asList(resultSet.getString("genres").split(","))),
+                new ArrayList<String>(Arrays.asList(resultSet.getString("followerIDs").split(","))),
+                new ArrayList<String>(Arrays.asList(resultSet.getString("followingIDs").split(",")))
+      	);
+        }
+      	
+        }
+      }
+      catch(SQLException e)
+      {
+        // if the error message is "out of memory",
+        // it probably means no database file is found
+        e.printStackTrace(System.err);
+      }
+      
+    }
+
+
+	public void listAllEvents() {
+		// TODO Auto-generated method stub
+		eventmanager.listAllEvents();
+	}
+
+
+	public void listAllUsers() {
+		usermanager.listAllUsers();
+		// TODO Auto-generated method stub
+		
+	}
 
 
 }
--- CS401LibrarySystem/src/com/library/management/UserManager.java
+++ CS401LibrarySystem/src/com/library/management/UserManager.java
@@ -1,114 +1,183 @@
 package com.library.management;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import com.library.management.EventsManager.Event;
 
 public class UserManager {
-    private String username;
-    private List<Book> favoriteBooks;
-    private String readingHabits;
-    private String literaryPreferences;
-    private List<UserManager> followers;
-    private List<UserManager> followings;
-    private int id;
+    private static ArrayList<User> users = new ArrayList<User>();  // Static list to hold all events
+    
+    
+    void createUser(int id, String username, ArrayList<String> favoriteBookISBN, ArrayList<String> readingHabits, ArrayList<String> genres, ArrayList<String> followerIDs, ArrayList<String> followingIDs) {
+        User user = new User(id, username, favoriteBookISBN, readingHabits, genres, followerIDs, followingIDs);
+        UserManager.users.add(user);  // Add this event to the list
+    }
+    
+    void createUser(String username) {
+    	User user = new User(username);
+    	UserManager.users.add(user);
+    }
+    
+    public ArrayList<User> getAllUsers() {
+        return new ArrayList<User>(users);  // Return a new ArrayList containing all events
+    }
+    
+    public static class User {
+    	private static int nextId = 1;
+        private String username;
+        private ArrayList<String> favoriteBookiSBN;
+        private ArrayList<String> readingHabits;
+        private ArrayList<String> genres;
+        private ArrayList<String> followerIDs;
+        private ArrayList<String> followingIDs;
+        private int id;
+    	
+        public User(String username) {
+            this.id = nextId++;
+            this.username = username;
+            this.favoriteBookiSBN = new ArrayList<String>();
+            this.readingHabits = new ArrayList<String>();
+            this.genres = new ArrayList<String>();
+            this.followerIDs = new ArrayList<String>();
+            this.followingIDs = new ArrayList<String>();
+        }
+        
+        public User(int id, String username, ArrayList<String> favoriteBookiSBN, ArrayList<String> readingHabits, ArrayList<String> genres, ArrayList<String> followerIDs, ArrayList<String> followingIDs) {
+            this.id = id;
+        	this.username = username;
+            this.favoriteBookiSBN = favoriteBookiSBN;
+            this.readingHabits = readingHabits;
+            this.genres = genres;
+            this.followerIDs = followerIDs;
+            this.followingIDs = followingIDs;
+            
+        }
+        
+        
+        public void addFavoriteBook(Book book) {
+        	favoriteBookiSBN.add(book.getISBN());
+        }
 
-    public UserManager(String username, int id) {
-        this.username = username;
-        this.id = id;
-        this.favoriteBooks = new ArrayList<>();
-        this.followers = new ArrayList<>();
-        this.followings = new ArrayList<>();
+        public void removeFavoriteBook(Book book) {
+        	favoriteBookiSBN.remove(book.getISBN());
+        }
+
+        public void postMessage(String message) {
+            // Logic for posting message
+        }
+
+        public void commentOnPost(String postId, String comment) {
+            // Logic for commenting on a post
+        }
+
+        public void likePost(String postId) {
+            // Logic for liking a post
+        }
+
+        public void sharePost(String postId) {
+            // Logic for sharing a post
+        }
+
+
+        public void participateInDiscussion(String discussion) {
+            // Logic for participating in a discussion
+        }
+        
+        public int getID() {
+        	return id;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public ArrayList<String> getFavoriteBookISBNs() {
+            return favoriteBookiSBN;
+        }
+        
+        public ArrayList<String> getReadingHabits() {
+            return readingHabits;
+        }
+        
+      public ArrayList<String> getGenres() {
+    	  return genres;
+      }
+//
+////        public void setFavoriteBooks(List<Book> favoriteBooks) {
+////            this.favoriteBooks = favoriteBooks;
+////        }
+//
+//        public String getReadingHabits() {
+//            return readingHabits;
+//        }
+//
+//        public void setReadingHabits(String readingHabits) {
+//            this.readingHabits = readingHabits;
+//        }
+//
+
+//
+//        public void setLiteraryPreferences(String literaryPreferences) {
+//            this.literaryPreferences = literaryPreferences;
+//        }
+
+        public List<String> getFollowers() {
+            return followerIDs;
+        }
+        
+        
+        
+
+//
+//        public void setFollowers(List<User> followers) {
+//            this.followers = followers;
+//        }
+//
+        public List<String> getFollowings() {
+            return followingIDs;
+        }
+//
+//        public void setFollowings(List<User> followings) {
+//            this.followings = followings;
+//        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+        
+        
+        @Override
+        public String toString() {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            return "Username: " + username + ", ID: " + id;
+        }
+        
+    	
     }
 
-    public void addFavoriteBook(Book book) {
-        favoriteBooks.add(book);
-    }
+	public void listAllUsers() {
+		// TODO Auto-generated method stub
+        if (users.isEmpty()) {
+            System.out.println("No Users.");
+            return;
+        }
+        for (User user : users) {
+            System.out.println(user.toString());
+        }
+		
+	}
 
-    public void removeFavoriteBook(Book book) {
-        favoriteBooks.remove(book);
-    }
-
-    public void postMessage(String message) {
-        // Logic for posting message
-    }
-
-    public void commentOnPost(String postId, String comment) {
-        // Logic for commenting on a post
-    }
-
-    public void likePost(String postId) {
-        // Logic for liking a post
-    }
-
-    public void sharePost(String postId) {
-        // Logic for sharing a post
-    }
-
-    public void joinGroup(GroupsManager.Groups group) {
-        group.addMember(this);
-    }
-
-    public void leaveGroup(GroupsManager.Groups group) {
-        group.removeMember(this);
-    }
-
-    public void participateInDiscussion(String discussion) {
-        // Logic for participating in a discussion
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public List<Book> getFavoriteBooks() {
-        return favoriteBooks;
-    }
-
-    public void setFavoriteBooks(List<Book> favoriteBooks) {
-        this.favoriteBooks = favoriteBooks;
-    }
-
-    public String getReadingHabits() {
-        return readingHabits;
-    }
-
-    public void setReadingHabits(String readingHabits) {
-        this.readingHabits = readingHabits;
-    }
-
-    public String getLiteraryPreferences() {
-        return literaryPreferences;
-    }
-
-    public void setLiteraryPreferences(String literaryPreferences) {
-        this.literaryPreferences = literaryPreferences;
-    }
-
-    public List<UserManager> getFollowers() {
-        return followers;
-    }
-
-    public void setFollowers(List<UserManager> followers) {
-        this.followers = followers;
-    }
-
-    public List<UserManager> getFollowings() {
-        return followings;
-    }
-
-    public void setFollowings(List<UserManager> followings) {
-        this.followings = followings;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
+   
 }
