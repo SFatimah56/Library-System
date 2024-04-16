d5714d49254eb879f711f26e3589197268bbf67e
sravyaravada23 <142958387+sravyaravada23@users.noreply.github.com>
2024-04-15 20:54:50 -0700

    Add files via upload

---
+++ CS401LibrarySystem/src/com/library/management/FollowingFollowers.java
@@ -0,0 +1,27 @@
+package com.library.management;
+
+import java.util.ArrayList;
+import java.util.List;
+	
+	//Followers class
+	class Followers {
+		private static ArrayList<User> followers = new ArrayList<>();
+		public Followers() {
+			Followers.followers = new ArrayList<>();
+		}
+		public void addFollower(User follower) {
+			followers.add(follower);
+		}
+	}
+	//Following class
+	class Following {
+		private static ArrayList<User> followings = new ArrayList<>();	
+
+	    public Following() {
+	        Following.followings = new ArrayList<>();
+	    }
+
+	    public void addFollowing(User following) {
+	        followings.add(following);
+	    }
+	}
